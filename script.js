832450cd5d5e0946fe7d62551d14bb5f6598e69f
Maxim <ya.max-m2013@yandex.ru>
2018-06-03 13:11:10 +0500

    Fixed

--- script.js
+++ script.js
@@ -6,36 +6,36 @@ canvas.style.left = '0px';
 canvas.width = document.body.offsetWidth;
 canvas.height = window.innerHeight;
 var sky = canvas.getContext('2d');
-var deg = Math.PI / 180;
+var radiansPerDegrees = Math.PI / 180;
 
-function edge(n, len) {
+function edge(n, length) {
   sky.save();
-  if (n == 0) {      
-    sky.lineTo(len, 0);
+  if (n === 0) {      
+    sky.lineTo(length, 0);
   } else {
     sky.scale(1 / 3, 1 / 3);   
-    edge(n - 1, len);
-    sky.rotate(60 * deg);
-    edge(n - 1, len);
-    sky.rotate(-120 * deg);
-    edge(n - 1, len);
-    sky.rotate(60 * deg);
-    edge(n - 1, len);
+    edge(n - 1, length);
+    sky.rotate(60 * radiansPerDegrees);
+    edge(n - 1, length);
+    sky.rotate(-120 * radiansPerDegrees);
+    edge(n - 1, length);
+    sky.rotate(60 * radiansPerDegrees);
+    edge(n - 1, length);
   }
   sky.restore();      
-  sky.translate(len, 0); 
+  sky.translate(length, 0); 
 }
 
-function drawFlake(x, y, len, n, stroke) {
+function drawFlake(x, y, length, n, stroke) {
   sky.save();
   sky.strokeStyle = stroke;
   sky.translate(x, y);
   sky.moveTo(0, 0);
-  edge(n, len);
-  sky.rotate(-120 * deg);
-  edge(n, len);
-  sky.rotate(-120 * deg);
-  edge(n, len);
+  edge(n, length);
+  sky.rotate(-120 * radiansPerDegrees);
+  edge(n, length);
+  sky.rotate(-120 * radiansPerDegrees);
+  edge(n, length);
   sky.stroke();
   sky.restore();
 }
